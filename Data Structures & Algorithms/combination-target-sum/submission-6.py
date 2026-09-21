653947f0aac87352efe0ba8c5c3c7897719ea911
Rishita Dugar <rishitadugar@gmail.com>
2026-09-21 14:49:31 -0700

    Add: combination-target-sum - submission-6

---
+++ Data Structures & Algorithms/combination-target-sum/submission-6.py
@@ -0,0 +1,28 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        subsets = []
+
+        def helper(index, subsets, currSet, runningSum):
+            #early exit conditions
+            if runningSum > target:
+                return
+            
+            #base case
+            if runningSum == target:
+                subsets.append(currSet.copy())
+                return
+
+            #recursive condition
+            for j in range(index, len(nums)): #start at index to avoid duplicate values
+                if runningSum + nums[j] > target:
+                    return
+
+                currSet.append(nums[j])
+                helper(j, subsets, currSet, runningSum + nums[j])
+                #until you iterate to next value, can reuse j
+                currSet.pop()
+                #runningSum -= nums[j] not needed bc only adding it conditionally
+
+        helper(0, subsets, [], 0)
+        return subsets
