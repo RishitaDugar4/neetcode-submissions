65b60bbdbf205b89fd2d0958dae2fc3ea75f55cd
Rishita Dugar <rishitadugar@gmail.com>
2026-09-24 04:50:55 -0700

    Add: min-cost-climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        '''
+        top down approach -> use recursion
+        '''
+        dp = [-1] * (len(cost)+1)
+
+        def dfs(n, dp):
+            if n < 0:
+                return 0
+            if dp[n] != -1:
+                return dp[n]
+                
+            dp[n] = cost[n] + min(dfs(n-1, dp), dfs(n-2, dp))
+            return dp[n]
+
+        return min(dfs(len(cost)-1, dp), dfs(len(cost)-2, dp))
+            
